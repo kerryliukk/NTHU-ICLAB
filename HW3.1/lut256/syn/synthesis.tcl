4ab0a789067bc14dc2111f8ed16227996e5e89cf
leothebestcoder <leoshieh0hn@gmail.com>
2022-04-09 01:07:38 +0800

    add HW3.1

---
+++ HW3.1/lut256/syn/synthesis.tcl
@@ -0,0 +1,12 @@
+# set your TOPLEVEL here
+set TOPLEVEL "rop3_lut256"
+
+# change your timing constraint here
+set TEST_CYCLE 1.8
+
+source -echo -verbose 0_readfile.tcl 
+source -echo -verbose 1_setting.tcl 
+source -echo -verbose 2_compile.tcl 
+source -echo -verbose 3_report.tcl 
+
+exit
